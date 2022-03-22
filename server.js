5ee8763d33b01082c353823c5fcfea57b1522ccf
Pattarap0l <97444813+Pattarap0l@users.noreply.github.com>
2022-03-22 16:09:00 +0700

    Add files via upload

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 
 const express = require('express')
+const { use } = require('express/lib/application')
 const app = express()
 const mongoose = require('mongoose')
 
@@ -8,6 +9,11 @@ mongoose.connect(process.env.DATABASE_URL)
 const db = mongoose.connection
 db.on('error', (error) => console.error(error))
 db.once('open', () => console.log('Connected to Database'))
+
+app.use(express.json())
+
+const subcribersRouter = require('./routes/subscribers')
+app.use('/subscribers',subcribersRouter)
 
 app.get ("/", (req, res) => {
     res.send ("Hello Node.js REST Server");
